0b5fdb68ad8b8b95b5e4cadf41d83984b133c779
EduardoDevword <eduardomarmentini.dev@gmail.com>
2024-01-18 16:29:49 -0300

    Iniciado o desenvolvimento da API em Node.js

---
+++ server.js
@@ -0,0 +1,63 @@
+"use strict"
+
+function normalizePort(val){
+    const port = parseInt(val, 10);
+
+    if(isNaN(port)) {
+        return val;
+    }
+
+    if(port >= 0 ) {
+        return port
+    }
+
+    return false
+};
+
+function onError(error) {
+    if(error.syscall !== "listen"){
+        throw error;
+    }
+
+    const bind = typeof port === "string" ?
+        "Pipe " + port :
+        "Port " + port;
+        
+        switch (error.code) {
+            case "EACESS":
+                console.error(bind + " requires elevated privileges");
+                process.exit(1);
+                break
+            case "EADDRINUSE":
+                console.error(bind + " is already in use");
+                process.exit(1);
+                break
+            default:
+                throw error;
+        }
+}
+
+const http    = require("http");
+const debug   =  require("debug")("nodestr:server");
+const express = require("express");
+const { type } = require("os");
+
+const app = express();
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+const server = http.createServer(app);
+const router = express.Router();
+
+let route = router.get("/", (req, res, next) => {
+    res.status(200).send({
+        title: "Node Store API",
+        version: "0.0.1"
+    }); 
+})
+app.use("/", route)
+
+server.listen(port);
+server.onError("error", onError);
+
+console.log(`API rodando na porta ${port}`)
