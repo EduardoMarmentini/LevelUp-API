de1df8f35513837448f7eabeea5cf704bdf339a7
EduardoDevword <eduardomarmentini.dev@gmail.com>
2024-01-22 09:25:56 -0300

    instaldo os pacotes para uso do mongoDB e configurado sua conexãoo, inserido as models para produto

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@
 
 const express =  require("express");
 const bodyParser = require("body-parser");  
+const  mongoose  =  require("mongoose");
 
 const app =  express();
 const router = express.Router();
+
+// Conexao ao banco
+mongoose.connect("mongodb+srv://root:Master2208@nodestore.ayfnqxx.mongodb.net/?retryWrites=true&w=majority")
 
 // Carrega rotas
 const indexRoute = require("./routes/index-route");
